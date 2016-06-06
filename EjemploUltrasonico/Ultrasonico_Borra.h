8e3282e508ea89f8130cc28e226437e1819099f0
JuanCarranza-Borra <juan_chango97@hotmail.com>
2016-06-06 10:29:31 -0500

    Facil Definicion de Pines

--- EjemploUltrasonico/Ultrasonico_Borra.h
+++ EjemploUltrasonico/Ultrasonico_Borra.h
@@ -1,17 +1,14 @@
 class Ultrasonico{
   private: int _PinEcho,_PinTrigger;
   public: 
-   void attach(int _Echo,int _Trigger);
-   int GetDis(void);
-};
-
-void Ultrasonico::attach(int _Echo,int _Trigger)
- {
-  _PinEcho = _Echo;
+   Ultrasonico(int _Echo,int _Trigger){
+     _PinEcho = _Echo;
   _PinTrigger = _Trigger;
   pinMode(_Echo,INPUT);
   pinMode(_Trigger,OUTPUT);
- }
+   }
+   int GetDis(void);
+};
 
 int Ultrasonico::GetDis(void)
 {
